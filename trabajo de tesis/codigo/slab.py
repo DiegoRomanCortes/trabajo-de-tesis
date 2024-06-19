8bd5de505ee10c6a99a57a00a85f296848f74a0d
DiegoRomanCortes <diegoroman2601@ug.uchile.cl>
2024-06-19 17:13:26 -0400

    avance en teoria

--- trabajo de tesis/codigo/slab.py
+++ trabajo de tesis/codigo/slab.py
@@ -32,13 +32,36 @@ plt.style.use(['science'])
 fig, ax = plt.subplots(dpi=300)
 ax.plot(x, y1, label='pares')
 ax.plot(x, y2, label='impares')
+
+
+
 ax.plot(x, np.sqrt(R1**2-x**2), label=r'$V_1$')
 ax.plot(x, np.sqrt(R2**2-x**2), label=r'$V_2$')
 ax.plot(x, np.sqrt(R3**2-x**2), label=r'$V_3$')
 
+for i in range(len(x)):
+    if x[i] < R1:
+        if np.abs(np.sqrt(R1**2-x[i]**2) - y1[i]) < 1e-2:
+            ax.plot(x[i], y1[i], 'ro')
+        if np.abs(np.sqrt(R1**2-x[i]**2) - y2[i]) < 1e-2:
+            ax.plot(x[i], y2[i], 'ro')
+    if x[i] < R2:
+        if np.abs(np.sqrt(R2**2-x[i]**2) - y1[i]) < 1e-2:
+            ax.plot(x[i], y1[i], 'ro')
+        if np.abs(np.sqrt(R2**2-x[i]**2) - y2[i]) < 1e-2:
+            ax.plot(x[i], y2[i], 'ro')
+    if x[i] < R3:
+        if np.abs(np.sqrt(R3**2-x[i]**2) - y1[i]) < 1e-2:
+            ax.plot(x[i], y1[i], 'ro')
+        if np.abs(np.sqrt(R3**2-x[i]**2) - y2[i]) < 1e-2:
+            ax.plot(x[i], y2[i], 'ro')
+
 ax.set_ylim(0.0, 1.1*R3)
 ax.set_xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi, 5*np.pi/2, 3*np.pi])
 ax.set_xticklabels(['0', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$', r'$5\pi/2$', r'$3\pi$'])
+
+ax.set_yticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi, 5*np.pi/2, 3*np.pi])
+ax.set_yticklabels(['0', r'$\pi/2$', r'$\pi$', r'$3\pi/2$', r'$2\pi$', r'$5\pi/2$', r'$3\pi$'])
 
 ax.set_xlabel(r'$\alpha a$')
 ax.set_ylabel(r'$\beta a$')
