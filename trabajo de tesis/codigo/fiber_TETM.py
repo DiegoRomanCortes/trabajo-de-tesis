eb9e78d170934d025bfe76a7e7210c340949385b
DiegoRomanCortes <diegoroman2601@ug.uchile.cl>
2024-07-26 19:05:36 -0400

    comienza soluciones graficas TETM fibra

--- trabajo de tesis/codigo/fiber_TETM.py
+++ trabajo de tesis/codigo/fiber_TETM.py
@@ -14,15 +14,18 @@ n2 = n0 + dn2
 dn3 = 30e-3
 n3 = n0 + dn3
 
-a = 3e-6
+n1 = n3
+
+a = 5e-6
 wavelength = 730e-9
 k0 = 2 * np.pi / wavelength
 
-kz = np.linspace(k0*n0, k0*n1, 400)
+kz = np.linspace(k0*n0, k0*n1, 1000)
 l = 0
 
-alpha = np.sqrt((k0*n0)**2 - kz**2)
-beta = np.sqrt((kz)**2 - (k0*n1)**2)
+alpha = np.sqrt((k0*n1)**2 - kz**2)
+
+beta = np.sqrt((kz)**2 - (k0*n0)**2)
 
 plt.style.use(['science'])
 fig, ax = plt.subplots(dpi=300)
@@ -33,12 +36,17 @@ def j_prime(l, x):
 def k_prime(l, x):
     return 0.5 * (kn(l-1, x) + kn(l+1, x))
 
-lhs = (j_prime(l, alpha*a) / jv(l, alpha*a) + k_prime(l, beta*a) / kn(l, beta*a)) * (n1 * j_prime(l, alpha*a) / jv(l, alpha*a) + n0 * k_prime(l, beta*a) / kn(l, beta*a))
-rhs = l**2 * ((1/(alpha*a))**2 + (1/(beta*a))**2)**2 * (kz/k0)**2  
 
-ax.plot(kz, lhs, label="lhs")
-ax.plot(kz, rhs, label="rhs")
+lhs_TE = (j_prime(l, alpha*a) / (jv(l, alpha*a)* (alpha*a)) + k_prime(l, beta*a) /( kn(l, beta*a)*beta*a)) 
+lhs_TM = (n1**2 * j_prime(l, alpha*a) / (jv(l, alpha*a)* (alpha*a)) + n0**2 * k_prime(l, beta*a) /( kn(l, beta*a)*beta*a))
 
+kz /= k0
+
+ax.plot(kz, lhs_TE, label='TE')
+ax.plot(kz, lhs_TM, label='TM')
+ax.plot(kz, np.zeros_like(kz), 'k--')
+# ax.set_yscale('log')
+ax.set_ylim(-6, 6)
 
 fig.legend()
 fig.savefig('../media/fibergraphical.png')
