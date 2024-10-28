d3cfee361ea5cd7d460541c9c9120306e3722e8d
DiegoRomanCortes <diegoroman2601@ug.uchile.cl>
2024-10-28 17:50:37 -0300

    agrega bandas

--- trabajo de tesis/codigo/couplingCMT.py
+++ trabajo de tesis/codigo/couplingCMT.py
@@ -10,7 +10,7 @@ dn3 = 1e-3
 n3 = n0 + dn3
 
 a = 3e-6
-d = 15e-6
+d = 12e-6
 wavelength = 730e-9
 k0 = 2 * np.pi / wavelength
 
@@ -47,59 +47,54 @@ n_shape_2 = np.zeros_like(x_to_plot)
 n_shape_3 = np.zeros_like(x_to_plot)
 n_shape_4 = np.zeros_like(x_to_plot)
 
-n1 = 1.0
-n2 = 1.2
+n1 = n0
+n2 = n3
 for idx, xi in enumerate(x_to_plot):
     if np.abs(xi+d) < a:
         n_shape_1[idx] = n2
-    elif np.abs(xi+d) >= a and np.abs(xi+d) <  d/2:
-        n_shape_1[idx] = n1
     else:
-        n_shape_1[idx] = 0
+        n_shape_1[idx] = n1
     if np.abs(xi) < a:
         n_shape_2[idx] = n2
-    elif np.abs(xi) >= a and np.abs(xi) <  d/2:
-        n_shape_2[idx] = n1
     else:
-        n_shape_2[idx] = 0
+        n_shape_2[idx] = n1
     if np.abs(xi-d) < a:
         n_shape_3[idx] = n2
-    elif np.abs(xi-d) >= a and np.abs(xi-d) < d/2:
-        n_shape_3[idx] = n1
     else:
-        n_shape_3[idx] = 0
+        n_shape_3[idx] = n1
     if np.abs(xi-2*d) < a:
         n_shape_4[idx] = n2
-    elif np.abs(xi-2*d) >= a and np.abs(xi-2*d) <  d/2:
-        n_shape_4[idx] = n1
     else:
-        n_shape_4[idx] = 0
+        n_shape_4[idx] = n1
 
 n_shape = n_shape_1 + n_shape_2 + n_shape_3 + n_shape_4
 n_shape[n_shape < n1] = n1
 
-n_shape2 = n_shape_1**2 + n_shape_2**2 + n_shape_3**2 + n_shape_4**2
-n_shape2[n_shape2 < n1**2] = n1**2
+n_shape2 = n_shape_1**2 + n_shape_2**2 + n_shape_3**2 + n_shape_4**2 -3*n1**2
 colors = ['#0C5DA5', '#00B945', '#FF9500', '#FF2C00', '#845B97', '#474747', '#9e9e9e']
-ax.plot((x_to_plot)*1e6, (n_shape2 - n_shape_3**2), color=colors[0])
+ax.plot((x_to_plot)*1e6, (n_shape2 - n_shape_3**2), color=colors[0], label=r"$n^2-n_m^2$")
+
+ax2 = ax.twinx()
 for n in range(1):
     E1 = E_field(x_to_plot+d, n)
     E2 = E_field(x_to_plot, n)
     E3 = E_field(x_to_plot-d, n)
     E4 = E_field(x_to_plot-2*d, n)
 
-    ax.plot((x_to_plot)*1e6, scale*E1, color=colors[1])
+    ax2.plot((x_to_plot)*1e6, E2, color=colors[1], label=r"$E_{m\prime}$")
     # ax.plot((x_to_plot)*1e6, scale*E2, color=colors[2])
-    ax.plot((x_to_plot)*1e6, scale*E3, color=colors[3])
+    ax2.plot((x_to_plot)*1e6, E3, color=colors[3], label=r"$E_{m}$")
     # ax.plot((x_to_plot)*1e6, scale*E4, color=colors[4])
     
     # ax.plot(x_to_plot*1e6, kz[n]/k0*np.ones_like(x_to_plot), "--", color=colors[n])
-    ax.plot((x_to_plot)*1e6,  E1*E3*300, color="black")
+    ax2.plot((x_to_plot)*1e6,  E2*E3, color="black", label=r"$E_{m^\prime}\cdot E_{m}$")
 
 ax.set_xlabel(r'$x$ ($\mu$m)')
-ax.set_ylabel(r'$n^2-n_m^2, E_{m\prime}, E_{m}$')
-ax.set_yticks([])
+ax.set_ylabel(r'$n^2-n_m^2$')
+ax2.set_ylabel(r'$E_{m\prime}, E_{m}$')
+# ax.set_yticks([])
 # ax.set_yticklabels(["$n_0$", "$n_1$"])
+fig.legend(loc='upper center', ncol=4, fontsize='x-small')
 fig.savefig('../media/coupling.pdf', bbox_inches='tight')
 
 
