e217781ab63efc95ea255d7d210230969200cb11
DiegoRomanCortes <diego.roman.c@ug.uchile.cl>
2025-03-04 09:51:27 -0300

    hamiltoniano spssh

--- trabajo de tesis/codigo/slm_blaze.py
+++ trabajo de tesis/codigo/slm_blaze.py
@@ -1,4 +1,20 @@
- 
+#    Copyright (C) 2024  Diego Roman-Cortes
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    e-mail: diego.roman.c@ug.uchile.cl
+
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image, ImageChops
@@ -11,17 +27,10 @@ plt.style.use('dark_background')
 WIDTH = 1920
 HEIGHT = 1080
 
-a = 6 # rango de parametrizacion
-b = 0.3
-sigma = HEIGHT/4#HEIGHT/15#2.2
+sigma = HEIGHT/4
+
 sigmax = HEIGHT/2.2/4*2.2
-
-sigmay = HEIGHT/5.0*1.1#*2.5*0
-
-sigma = HEIGHT/8
-n = 0
-l = 2
-num = 1
+sigmay = HEIGHT/5.0*1.1
 
 x = np.linspace(-WIDTH/2, WIDTH/2, num=WIDTH)
 y = np.linspace(-HEIGHT/2, HEIGHT/2, num=HEIGHT)
@@ -32,39 +41,13 @@ angle = -0.05
 X = Xn*np.cos(angle) - Yn*np.sin(angle)
 Y = Yn*np.cos(angle) + Xn*np.sin(angle)
 
-angle = 10*np.pi/45
-
-R = np.sqrt(X**2 + Y**2)
-PHI = np.arctan2(Y, X)
 Z = np.zeros(X.shape, dtype=complex)
-
-A = 1.0
-sigmax *= 0.75
-sigma /= 1.5
-
-sigmay = sigmax*1.9
-sigmax = sigma*1.9
-sigma *= 1.3
-dy = -sigma*1.5*0
-dx1 = sigma*0.1
-dx2 = -sigma*0.4*0
-
-per = 100
-kick = np.pi/2*0
-alpha = 0.3
-
-
-
-Z += (np.exp(-((X)/sigma/1.0)**2)*np.exp(-((Y)/sigma)**2))
-
-PHI = np.arctan2(Y, X)
-
-sigma *= 3
-Z = (R*np.sqrt(2)/sigma)**np.abs(l) * np.exp(-(R/sigma)**2) * eval_genlaguerre(n, np.abs(l), 2*R/sigma) * np.exp(-1j*l*PHI)
-
+Z += (np.exp(-((X)/sigma/1.0)**2)*np.exp(-((Y)/sigma)**2)) # any function of x and y
 Z /= np.sqrt(np.sum(np.abs(Z)**2))
 
 phase = (np.angle(Z)+np.pi) * 255.0 / (2*np.pi)
+
+fig = plt.figure(figsize=(1920/my_dpi, 1080/my_dpi), dpi=my_dpi)
 
 plt.imsave('fase.png', phase, cmap="gray", vmin=0, vmax=255)
 plt.close("all")
