9ba5ed24572ce5736a0534bcbb8cff269bfc31e6
DiegoRomanCortes <diego.roman.c@ug.uchile.cl>
2025-04-22 17:10:18 -0400

    termina figura invcoup

--- trabajo de tesis/codigo/pcouplers.py
+++ trabajo de tesis/codigo/pcouplers.py
@@ -1,71 +1,62 @@
-from matplotlib.colors import LinearSegmentedColormap
 import numpy as np
-import cupy as cp
-import matplotlib.pyplot as plt
-from cupyx.scipy.sparse import diags, linalg, spdiags, kronsum
-import scienceplots
-from cmcrameri import cm
+from scipy.sparse import diags, linalg, spdiags, kronsum
 
 # Define the parameters
-Nx = 120
-Ny = 120
+Nx = 200
+Ny = 200
 Lx = 60E-6
 Ly = 60E-6
 
+x, dx = np.linspace(-Lx/2, Lx/2, Nx, retstep=True)
+y, dy = np.linspace(-Ly/2, Ly/2, Ny, retstep=True)
 
-dx = Lx / (Nx - 1)
-dy = Ly / (Ny - 1)
-
-x = cp.linspace(-Lx/2, Lx/2, Nx)
-y = cp.linspace(-Ly/2, Ly/2, Ny)
-
-X, Y = cp.meshgrid(x, y, indexing="xy")
+X, Y = np.meshgrid(x, y, indexing="xy")
 
 
 n0 = 1.48  # Refractive index of the background
-dn1 = 8.0E-4  # 4.4 Amplitude of the refractive index modulation
+dn1 = 13E-4  # 4.4 Amplitude of the refractive index modulation
 wavelength = 730E-9  # Wavelength of the light
-k0 = 2 * cp.pi / wavelength  # Wavenumber
+k0 = 2 * np.pi / wavelength  # Wavenumber
 
-angles = cp.linspace(0, np.pi/2, 10)
+angles = np.linspace(0, np.pi/2, 50)
 
+np.save("angles.npy", angles)
 
-n_eigen = 6 
-eigenvalues_tot = cp.zeros((len(angles), n_eigen))
-eigenvectors_tot = cp.zeros((len(angles), Nx*Ny, n_eigen))
+n_eigen = 4
+eigenvalues_tot = np.zeros((len(angles), n_eigen))
+eigenvectors_tot = np.zeros((len(angles), Nx*Ny, n_eigen))
 
-wx1 = 1.4E-6  # Width of the refractive index modulation
+wx1 = 1.2E-6  # Width of the refractive index modulation
 wy1 = 3.1E-6
 
-a1 = 18E-6
-a2 = 18E-6
+a1 = 25E-6
 
 ##create matrix
-diagx = cp.ones([Nx])
-diagy = cp.ones([Ny])
+diagx = np.ones([Nx])
+diagy = np.ones([Ny])
 
-diagsx = cp.array([diagx, -2*diagx, diagx])
-diagsy = cp.array([diagy, -2*diagy, diagy])
+diagsx = np.array([diagx, -2*diagx, diagx])
+diagsy = np.array([diagy, -2*diagy, diagy])
 Dx = spdiags(diagsx, np.array([-1, 0, 1]), Nx, Nx)/(dx**2)
 Dy = spdiags(diagsy, np.array([-1, 0, 1]), Ny, Ny)/(dy**2)
 
 def dn_func(X, Y, wx, wy):
-    output = cp.tanh(33.0*cp.exp(-(X/wx)**2- ((Y)/wy)**2))
+    output = np.tanh(33.0*np.exp(-(X/wx)**2- ((Y)/wy)**2))
     return output
 
 zmin = 5E-3
 zmax = 25E-3
 
-zarr = cp.linspace(zmin, zmax, 1000)
+zarr = np.linspace(zmin, zmax, 1000)
 
-propagation_dynamics = cp.zeros((len(zarr), len(angles), Nx), dtype=cp.complex128)
+propagation_dynamics = np.zeros((len(zarr), len(angles), Nx), dtype=np.complex128)
 
 for idx, angle in enumerate(angles):
     print(f"Angle: {angle}")
-    dn_array = cp.zeros(X.shape)
+    dn_array = np.zeros(X.shape)
 
-    dn_array += dn1 * dn_func(X+a1*cp.cos(angle)/2, Y+a1*cp.sin(angle)/2, wx1, wy1)
-    dn_array += dn1 * dn_func(X-a1*cp.cos(angle)/2, Y-a1*cp.sin(angle)/2, wx1, wy1)
+    dn_array += dn1 * dn_func(X+a1*np.cos(angle)/2, Y+a1*np.sin(angle)/2, wx1, wy1)
+    dn_array += dn1 * dn_func(X-a1*np.cos(angle)/2, Y-a1*np.sin(angle)/2, wx1, wy1)
 
     # Create the sparse matrix for the Helmholtz problem
     n = n0 + dn_array
@@ -79,4 +70,4 @@ for idx, angle in enumerate(angles):
     eigenvalues_tot[idx, :] = eigenvalues
     eigenvectors_tot[idx, :, :] = eigenvectors
 
-    cp.save(f"eigenvalues_{idx}.npy", eigenvalues)
+    np.save(f"eigenvalues_{idx}.npy", eigenvalues)
