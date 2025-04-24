99b2cb356040d473976a027dbe0c4bf8a65a19db
DiegoRomanCortes <diego.roman.c@ug.uchile.cl>
2025-04-24 17:22:43 -0400

    avanza en inv

--- trabajo de tesis/codigo/pcouplers.py
+++ trabajo de tesis/codigo/pcouplers.py
@@ -2,10 +2,10 @@ import numpy as np
 from scipy.sparse import diags, linalg, spdiags, kronsum
 
 # Define the parameters
-Nx = 200
-Ny = 200
-Lx = 60E-6
-Ly = 60E-6
+Nx = 240
+Ny = 240
+Lx = 80E-6
+Ly = 80E-6
 
 x, dx = np.linspace(-Lx/2, Lx/2, Nx, retstep=True)
 y, dy = np.linspace(-Ly/2, Ly/2, Ny, retstep=True)
@@ -14,7 +14,7 @@ X, Y = np.meshgrid(x, y, indexing="xy")
 
 
 n0 = 1.48  # Refractive index of the background
-dn1 = 13E-4  # 4.4 Amplitude of the refractive index modulation
+dn1 = 1.530E-3  # 4.4 Amplitude of the refractive index modulation
 wavelength = 730E-9  # Wavelength of the light
 k0 = 2 * np.pi / wavelength  # Wavenumber
 
@@ -26,8 +26,8 @@ n_eigen = 4
 eigenvalues_tot = np.zeros((len(angles), n_eigen))
 eigenvectors_tot = np.zeros((len(angles), Nx*Ny, n_eigen))
 
-wx1 = 1.2E-6  # Width of the refractive index modulation
-wy1 = 3.1E-6
+wx1 = 0.9E-6  # Width of the refractive index modulation
+wy1 = 3.3E-6
 
 a1 = 25E-6
 
@@ -69,5 +69,8 @@ for idx, angle in enumerate(angles):
     eigenvalues, eigenvectors = linalg.eigsh(H, k=n_eigen, which='LA')
     eigenvalues_tot[idx, :] = eigenvalues
     eigenvectors_tot[idx, :, :] = eigenvectors
-
+    print(np.sqrt(eigenvalues[0])/k0 - n0)
+    print(np.sqrt(eigenvalues[1])/k0 - n0)
+    print(np.abs(np.sqrt(eigenvalues[1]) - np.sqrt(eigenvalues[0]))/2*1e-2)
+    print("\n")
     np.save(f"eigenvalues_{idx}.npy", eigenvalues)
--- trabajo de tesis/codigo/pcouplers_plot.py
+++ trabajo de tesis/codigo/pcouplers_plot.py
@@ -28,6 +28,11 @@ kz_temp = kz_plus[idx_magic:].copy()
 kz_plus[idx_magic:] = kz_minus[idx_magic:]
 kz_minus[idx_magic:] = kz_temp
 
+angles_exp = np.array([ 0,  1,  2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])*np.pi/40
+couplings_exp = np.array( [ 0.18982468,  0.19759136,  0.19167419,  0.18454876,  
+                           0.18354335, 0.14111277,  0.11345177,  0.07334573, -0.08385819, -0.13810501, 
+                           -0.22390854, -0.32961958, -0.44581551, -0.59109267, -0.6538189, -0.78539816, 
+                           -0.86393798, -0.93583845, -1.05301696, -1.13995956, -1.21760956])
 
 
 ax.plot(angles, kz_minus, 'r', label=r'$k_z^{+-}$')
@@ -35,14 +40,12 @@ ax.plot(angles, kz_plus, 'g', label=r'$k_z^{++}$')
 ax2.plot(angles, couplings, 'k-', label=r'$\Delta k_z / 2$')
 
 ax2.hlines(y=0, xmin=angles[0], xmax=angles[-1], color='k', linestyle='--', linewidth=0.5)
-ax2.vlines(angles[idx_magic], ymin=couplings.min(), ymax=couplings.max(), color='k', linestyle='--', linewidth=0.5)
+ax2.vlines(angles[idx_magic], ymin=couplings_exp.min(), ymax=couplings_exp.max(), color='k', linestyle='--', linewidth=0.5)
 
 ax2.text(angles[idx_magic-1], couplings.min(), r"$\theta_m$", ha="center", va="bottom", fontsize=14)
 
 ax2.set_ylabel(r'Coupling (cm$^{-1}$)')
 
-angles_exp = np.array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])*np.pi/40
-couplings_exp = np.array([ 0.18982468,  0.19759136,  0.19167419,  0.18454876,  0.18354335, 0.14111277,  0.11345177,  0.07334573, -0.08385819, -0.13810501, -0.22390854, -0.32961958, -0.44581551, -0.59109267, -0.6538189, -0.78539816, -0.86393798, -0.93583845, -1.05301696, -1.13995956, -1.21760956])
 
 ax2.plot(angles_exp, couplings_exp, 'k.', label='Exp. data')
 ax.ticklabel_format(axis='y', style='sci')
